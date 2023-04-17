52d75142c33536078b0183b404b2a0f2401e7a5a
ssari-memory <sercan.sari@std.yeditepe.edu.tr>
2023-04-16 23:05:22 -0700

    minor fixes

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -15,7 +15,7 @@ if [ ! -d "$filesdir" ]; then
 fi
 
 #find the number of files and the number of matching lines
-num_files=$(find "$filesdir" -type -f | wc -l)
+num_files=$(find "$filesdir" -type f| wc -l) 
 num_matching_lines=$(grep -r "$searchstr" "$filesdir" | wc -l)
 
 echo "The number of files are $num_files and the number of matching lines are $num_matching_lines"
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -6,8 +6,8 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-$writefile="$1"
-$writestr="$2"
+writefile="$1"
+writestr="$2"
 
 mkdir -p "$(dirname "$writefile")"
 
